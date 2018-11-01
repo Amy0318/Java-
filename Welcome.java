8aa6c7b88e2821b8d65850aa2f8a72e84034f7a9
Amy0318 <44562304+Amy0318@users.noreply.github.com>
2018-11-01 09:04:08 +0800

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome{
+  public static void main(String[]args) {
+  //Display mesage Welcome to Java!on the console
+  System,out.println("Welcome to Java!");
+  }
+}
+1.第一行定义了一个类。每个Java程序至少应该有一个类。每个类都有一个名字。按照惯例，类名都是以大写字母开头的。
+2.第二行定义main方法。程序是从main方法开始执行的。一个类可以包含几个方法。main方法是程序开始执行的入口。
